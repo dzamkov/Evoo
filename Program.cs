ceb6d06cc5c2c22dd1463d4577103d3bd5ab0993
Dmitry Zamkov <dmitry94@gmail.com>
2010-08-08 09:58:44 -0400

    Green screen

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Evoo
+{
+    public static class Program
+    {
+        /// <summary>
+        /// Application main entry point.
+        /// </summary>
+        public static void Main(string[] Args)
+        {
+            Window w = new Window();
+            w.Run();
+        }
+    }
+}
---
+++ Window.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Evoo
+{
+    /// <summary>
+    /// Main program window.
+    /// </summary>
+    public class Window : GameWindow
+    {
+        public Window()
+            : base(640, 480, OpenTK.Graphics.GraphicsMode.Default, "Evoo")
+        {
+
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.ClearColor(0.2f, 0.8f, 0.2f, 1.0f);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            this.SwapBuffers();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            
+        }
+    }
+}
